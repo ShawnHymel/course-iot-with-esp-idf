97867f89a5ad413d2d6ef97c51b21f4efa3bbe45
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2025-01-23 11:57:16 -0700

    Fixed up DNS lookup to support IPv4 and IPv6

--- workspace/http_request/main/main.c
+++ workspace/http_request/main/main.c
@@ -7,16 +7,37 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+
 #include "lwip/err.h"
+#include "lwip/sockets.h"
 #include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
 
 #include "wifi_settings.h"
 
-// FreeROTS event group
-static EventGroupHandle_t s_wifi_event_group;
+// Server settings and URL to fetch
+#define WEB_HOST "example.com"
+#define WEB_PORT "80"
+#define WEB_PATH "/"
+
+// HTTP GET request
+static const char *REQUEST = "GET " WEB_PATH " HTTP/1.0\r\n"
+    "Host: "WEB_HOST":"WEB_PORT"\r\n"
+    "User-Agent: esp-idf/1.0 esp32\r\n"
+    "\r\n";
+
+// Log level for this file
+#define LOG_LEVEL ESP_LOG_VERBOSE
+
+// Set IPv4 or IPv6 family (AF_INET or AF_INET6)
+#define FAMILY AF_INET6
 
 // Tag for debug messages
-static const char *TAG = "WiFi STA";
+static const char *TAG = "http_request";
+
+// FreeROTS event group
+static EventGroupHandle_t s_wifi_event_group;
 
 // Called on WiFi events
 static void wifi_event_handler(void* arg, 
@@ -32,8 +53,8 @@ static void wifi_event_handler(void* arg,
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
         xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
         ESP_LOGI(TAG, 
-            "WiFi disconnected. Waiting %i seconds to reconnect.", 
-            CONFIG_ESP_RECONNECT_DELAY_SEC);
+                 "WiFi disconnected. Waiting %i seconds to reconnect.", 
+                  CONFIG_ESP_RECONNECT_DELAY_SEC);
         vTaskDelay((CONFIG_ESP_RECONNECT_DELAY_SEC * 1000) / portTICK_PERIOD_MS);
         ESP_LOGI(TAG, "Reconnecting to %s...", ESP_WIFI_SSID);
         esp_wifi_connect();
@@ -118,12 +139,158 @@ void wifi_init_sta(void)
     }
 }
 
+// Perform DNS lookup
+esp_err_t dns_lookup(const char *host, 
+                     const char *port,
+                     const int family,
+                     struct addrinfo *ip_addr)
+{
+    struct addrinfo *res;
+    char ip_str[INET6_ADDRSTRLEN] = {0};
+    int err;
+
+    // Check for supported address families
+    if ((family != AF_INET) && (family != AF_INET6) && (family != AF_UNSPEC)) {
+        ESP_LOGE(TAG, "Unsupported address family");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    // Set hints for DNS lookup
+    struct addrinfo hints = {
+        .ai_family = family,
+        .ai_socktype = SOCK_STREAM
+    };
+
+    // Perform DNS lookup
+    err = getaddrinfo(host, port, &hints, &res);
+    if (err != 0 || res == NULL) {
+        ESP_LOGE(TAG, "DNS lookup failed (%d) res: %p", err, res);
+        return ESP_FAIL;
+    }
+
+    // Print resolved IP addresses (linked list)
+    ESP_LOGI(TAG, "DNS lookup succeeded. IP addresses:");
+    for (struct addrinfo *addr = res; addr != NULL; addr = addr->ai_next) {
+        if (addr->ai_family == AF_INET) {
+            struct in_addr *ip = &((struct sockaddr_in *)addr->ai_addr)->sin_addr;
+            inet_ntop(AF_INET, ip, ip_str, INET_ADDRSTRLEN);
+            ESP_LOGI(TAG, "  IPv4: %s", ip_str);
+        }
+        else if (addr->ai_family == AF_INET6) {
+            struct in6_addr *ip = &((struct sockaddr_in6 *)addr->ai_addr)->sin6_addr;
+            inet_ntop(AF_INET6, ip, ip_str, INET6_ADDRSTRLEN);
+            ESP_LOGI(TAG, "  IPv6: %s", ip_str);
+        }
+    }
+
+    // Copy first resolved IP address to output struct
+    if (ip_addr && res) {
+        memcpy(ip_addr, res, sizeof(struct addrinfo));
+    }
+
+    // Free address info
+    freeaddrinfo(res);
+
+    return ESP_OK;
+}
+
+static void http_get_task(void *pvParameters)
+{
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
+    int s, r;
+    char recv_buf[64];
+
+    while(1) {
+        int err = getaddrinfo(WEB_HOST, WEB_PORT, &hints, &res);
+
+        if(err != 0 || res == NULL) {
+            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        /* Code to print the resolved IP.
+
+           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
+        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+        s = socket(res->ai_family, res->ai_socktype, 0);
+        if(s < 0) {
+            ESP_LOGE(TAG, "... Failed to allocate socket.");
+            freeaddrinfo(res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... allocated socket");
+
+        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+            close(s);
+            freeaddrinfo(res);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        ESP_LOGI(TAG, "... connected");
+        freeaddrinfo(res);
+
+        if (write(s, REQUEST, strlen(REQUEST)) < 0) {
+            ESP_LOGE(TAG, "... socket send failed");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... socket send success");
+
+        struct timeval receiving_timeout;
+        receiving_timeout.tv_sec = 5;
+        receiving_timeout.tv_usec = 0;
+        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                sizeof(receiving_timeout)) < 0) {
+            ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... set socket receiving timeout success");
+
+        /* Read HTTP response */
+        do {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf)-1);
+            for(int i = 0; i < r; i++) {
+                putchar(recv_buf[i]);
+            }
+        } while(r > 0);
+
+        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
+        close(s);
+        for(int countdown = 10; countdown >= 0; countdown--) {
+            ESP_LOGI(TAG, "%d... ", countdown);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        ESP_LOGI(TAG, "Starting again!");
+    }
+}
+
 // Main app entrypoint
 void app_main(void)
 {
     esp_err_t ret;
+    struct addrinfo ip_addr;
 
-    // Say something
+    // Set log levels
+    esp_log_level_set("wifi", LOG_LEVEL);
+    esp_log_level_set(TAG, LOG_LEVEL);
+
+    // Welcome message (after delay to allow serial connection)
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
     ESP_LOGI(TAG, "Starting HTTP GET request demo");
 
     // Initialize NVS: ESP32 WiFi driver uses NVS to store WiFi settings
@@ -137,8 +304,26 @@ void app_main(void)
     // Initialize WiFi in station mode
     wifi_init_sta();
 
+    // Create task to perform HTTP GET request
+    // xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+
+    // Perform DNS lookup
+    ret = dns_lookup(WEB_HOST, WEB_PORT, FAMILY, &ip_addr);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "DNS lookup failed");
+        return;
+    }
+
+    // Create task to perform HTTP GET request
+
+    // TODO:
+    // - DNS lookup function
+    // - HTTP GET request function
+
     // Superloop: do nothing
     while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS); 
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        // ESP_LOGI(TAG, "Disconnecting from AP...");
+        // esp_wifi_disconnect();
     }
 }
